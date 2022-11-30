34edd2b3c37bcd8b3d19715286aee45e5dbfbab1
junqing_Gitkraken <gwills@163.com>
2022-11-30 12:36:39 +0800

    fix outpath display bug

--- build/lib/pyCombiner/__init__.py
+++ build/lib/pyCombiner/__init__.py
@@ -6,7 +6,7 @@ def main():
     return "hello"
 
 
-version = "0.1.0"
+version = "0.1.1"
 description = "Combine that all your python files " \
               "in your project sequential " \
               "into one by the relationship of import " \
